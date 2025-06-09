b27f29fec68aa27e8c9f563ff3bd43af97e9f065
sakshi20024 <88770674+sakshi20024@users.noreply.github.com>
2025-06-09 12:25:55 +0530

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserRetailEBillDisplay.Models;
+
+namespace UserRetailEBillDisplay.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Invoice> Invoices { get; set; } = null!;
+    }
+}
